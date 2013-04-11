db1c88d4fa58cc62a463c609fef791a324acdbd4
Plattenschieber <plattenschieber@gmail.com>
2013-04-12 01:28:43 +0200

    linked list definitions

---
+++ particle.h
@@ -0,0 +1,24 @@
+#ifndef MAIN_H
+#define MAIN_H
+#define DIM 2
+
+typedef struct {
+	int ID;
+	double x[DIM];
+	double v[DIM];
+} PARTICLE;
+
+typedef struct NODE {
+	void * data;
+	struct NODE * next;
+} NODE;
+
+typedef struct {
+	NODE * first;
+} LIST;
+
+/* delete a node from the list */
+void list_delete(LIST * L, NODE * del);
+void list_free(LIST * L);
+
+#endif
