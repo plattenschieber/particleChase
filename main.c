ce96acaa0a6deacdbcf488b0854d753c2f1c0458
Plattenschieber <plattenschieber@gmail.com>
2013-04-12 01:28:14 +0200

    some basic tests with a linked list of particles

--- main.c
+++ main.c
@@ -1,7 +1,23 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include "particle.h"
 
-int main()
-{
-	printf("Hello World\n");
+int main() {
+	int i;
+	PARTICLE particle;
+	NODE *p;
+	LIST *L;
+	particle.x[0] = 1.3; particle.x[1] = 2.3;
+	particle.ID = 2;
+
+	/* loop through the list L */
+	//for (p = L->first; p; p = p->next) {
+		/* code */
+	
+
+	for (i = 0; i < DIM; i++) {
+		printf("%f\n",particle.x[i]);
+	}
+	printf("%i\n",particle.ID);
 	return 0;
 }
