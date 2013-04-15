80976f5380334f84ed7e2d9033bdff8b7a9fe10a
Plattenschieber <plattenschieber@gmail.com>
2013-04-15 18:34:35 +0200

    add create, insert and free function header

--- particle.h
+++ particle.h
@@ -20,8 +20,13 @@ typedef struct {
 	NODE *first;
 } LIST;
 
-void list_free(LIST * L);
+/* create a new node with 'data' and return its adress */
+NODE *list_create(LIST *L, void *data);
+/* insert a new node with 'data' behind 'cursor' and reset 'L->first' if 'cursor==NULL' */
+NODE *list_insert(LIST *L, NODE *cursor, void *data);
 /* delete the 'cursor' from the list and leave everything clean */
 void list_delete(LIST *L, NODE *cursor);
+/* delete every element of the list  */
+void list_free(LIST *L);
 
 #endif
