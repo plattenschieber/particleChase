7f3640a59f4448fa8aecdc029d730f0ec9b356e2
Plattenschieber <plattenschieber@gmail.com>
2013-04-15 18:29:43 +0200

    add two particles to the list

--- main.c
+++ main.c
@@ -8,7 +8,11 @@ int main() {
 	LIST *L;
 	particle.x[0] = 1.3; particle.x[1] = 2.3;
 	particle.ID = 2;
+	L->first = list_create(L, &particle);
 
+	particle.x[0] = 1.5; particle.x[1] = 4.8;
+	particle.ID = 1;
+	list_insert(L, L->first, &particle);
 
 	}
 	return 0;
