eda8641987143277a63f6767782fd66ebc2679ca
Plattenschieber <plattenschieber@gmail.com>
2013-04-15 18:28:33 +0200

    remove some example code

--- main.c
+++ main.c
@@ -5,19 +5,11 @@
 int main() {
 	int i;
 	PARTICLE particle;
-	NODE *p;
 	LIST *L;
 	particle.x[0] = 1.3; particle.x[1] = 2.3;
 	particle.ID = 2;
 
-	/* loop through the list L */
-	//for (p = L->first; p; p = p->next) {
-		/* code */
-	
 
-	for (i = 0; i < DIM; i++) {
-		printf("%f\n",particle.x[i]);
 	}
-	printf("%i\n",particle.ID);
 	return 0;
 }
