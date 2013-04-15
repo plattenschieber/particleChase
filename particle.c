a5eba76d698240091247517797474adccbc879e4
Plattenschieber <plattenschieber@gmail.com>
2013-04-15 18:42:23 +0200

    update list_insert with error handling

--- particle.c
+++ particle.c
@@ -1,12 +1,5 @@
 #include "particle.h"
 
-/* insert data right behind cursor. If cursor equals NULL, insert at the beginning. Return inserted node or NULL if an errror occurs */
-NODE * list_insert(LIST * L, NODE * cursor, double data) {
-	NODE * insert;
-	if ( (insert=malloc(sizeof(NODE))) != NULL ) {
-		insert->data = &data;
-		insert->next = cursor->next;
-		cursor->next = insert;
 NODE *list_create(LIST *L, void *data) {
 	NODE *node;
 	if( !(node=malloc(sizeof(NODE))) ) return NULL;
@@ -15,6 +8,17 @@ NODE *list_create(LIST *L, void *data) {
 	return node;
 }
 
+NODE *list_insert(LIST *L, NODE *cursor, void *data) {
+	NODE *newnode;
+	if( (newnode=list_create(L, data)) ) {
+        newnode->next = cursor->next;
+        cursor->next = newnode;
+		return newnode;
 	}
-	return insert;
+	else {
+		fprintf(stderr, "Could not allocate memory for new node\n");
+        exit(EXIT_FAILURE); 
+	}
+}
+
 }
