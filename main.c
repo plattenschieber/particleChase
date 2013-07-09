ad6bf8268fac8228101aec199b4fdc602359deb8
Plattenschieber <plattenschieber@gmail.com>
2013-07-09 23:08:51 +0200

    comment update

--- main.c
+++ main.c
@@ -8,11 +8,12 @@ int main() {
    	PARTICLE p2;
 	WORLD *W = world_init(NULL);
 
-	/* insert some particles */
+	/* insert first particle */
 	p1.x[0] = 1.3; p1.x[1] = 2.3;
 	p1.ID = 2;
 	list_insert(W->cells, NULL, &p1);
 
+	/* insert second particle on first place */
 	p2.x[0] = 1.5; p2.x[1] = 4.8;
 	p2.ID = 1;
 	list_insert(W->cells,W->cells->first, &p2);
