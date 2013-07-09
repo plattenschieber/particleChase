fb4847cea0c79e34d6e48ab36f7443a67ba2a50f
Plattenschieber <plattenschieber@gmail.com>
2013-07-09 23:09:49 +0200

    update world_init() for WORLD struct

--- world.c
+++ world.c
@@ -2,13 +2,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+WORLD *world_init(FILE *F) {
+	WORLD *W;
+	if( !(W=malloc(sizeof(WORLD))) ) {
+		fprintf(stderr, "Could not allocate memory for the World\n");
+		exit(EXIT_FAILURE); 
 	}
-}
-void world_init(FILE *F) {
 	/* read from File instead */
-	world_t = 0.0;
-	world_t_end = 2.0;
-	world_delta_t = 0.1;
+	W->t = 0.0;
+	W->t_end = 2.0;
+	W->delta_t = 0.1;
+	W->cells = list_init();
+	W->step = 0;
+	W->n_particles = 0;
+	return W;
+}
+
 void simulate(WORLD *W) {
 	while(W->t < W->t_end) {
 		update_x(W);
