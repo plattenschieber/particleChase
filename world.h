e3f03de57303b233271be0d35628c8fe6dbb5a96
Plattenschieber <plattenschieber@gmail.com>
2013-07-09 21:44:34 +0200

    move variables into the WORLD struct

--- world.h
+++ world.h
@@ -2,19 +2,21 @@
 #define WORLD_H
 #include "particle.h"
 #include <stdio.h> 
+typedef struct {
+	/* a list of cells with particle lists */
+	LIST *cells;
+	// current time */
+	double t;
+	// timestep */
+	double delta_t;
+	// end time */
+	double t_end;
+	// number of overall particles */
+	unsigned int n_particles;
+	/* actual step */
+	unsigned int step;
+} WORLD;
 
-/* a list of cells with particle lists */
-LIST *cells;
-// current time */
-double world_t;
-// timestep */
-double world_delta_t;
-// end time */
-double world_t_end;
-// number of overall particles */
-unsigned int n_particles;
-/* actual step */
-unsigned int world_step;
 /* start the simulation */
 void simulate(LIST *L);
 /* update the positions */
