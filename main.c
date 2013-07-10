a7b499715f72c3fb2be3beda3579ce81fec90074
Plattenschieber <plattenschieber@gmail.com>
2013-07-10 18:18:49 +0200

    file handling for input files

--- main.c
+++ main.c
@@ -25,9 +25,21 @@ int main(int argc, char **argv) {
 	else 
 		printf("Too many arguments.\n");
 
+	/* open file handling */
+	FILE *parameter, *particles; 
+	if ((parameter = fopen(file1,"r"))==NULL) {
+		fprintf(stderr, "Could not open %s. Please Check your file.\n", file1);
+		exit(EXIT_FAILURE); 
+	}
+	if ((particles = fopen(file2,"r"))==NULL) {
+		fprintf(stderr, "Could not open %s. Please Check your file.\n", file2);
+		exit(EXIT_FAILURE); 
+	}
 
 	/* initialize the world and close files */
 	WORLD *W = world_init(parameter, particles);
+	fclose(parameter);
+	fclose(particles);
 	/* print particles before and after the simulation */ 
 	print_particles(W->cells);
 	simulate(W);
