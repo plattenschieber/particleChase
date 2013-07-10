2f3141f64165c2222ddfffd89ef599c4e796654a
Plattenschieber <plattenschieber@gmail.com>
2013-07-10 23:34:23 +0200

    change order of argument list -> particles before parameter

--- main.c
+++ main.c
@@ -9,14 +9,14 @@ int main(int argc, char **argv) {
 	char file1[100];
 	char file2[100];
 	if (argc == 1) {
-		strcpy(file1,"unit.parameter");
-		strcpy(file2,"unit.particles");
+		strcpy(file1,"unit.particles");
+		strcpy(file2,"unit.parameter");
 		printf("No parameter file, nor particles given - using 'unit.parameter' and 'unit.particles' instead\n");
 	}
 	else if (argc == 2) { 
 		strcpy(file1,argv[2]);
 		strcpy(file2, "unit.parameter");
-		printf("No parameter file given, using 'unit.parameter' instead\n");
+		printf("No particles file given, using 'unit.particles' instead\n");
 	}
 	else if (argc == 3) { 
 		strcpy(file1,argv[2]);
@@ -27,12 +27,12 @@ int main(int argc, char **argv) {
 
 	/* open file handling */
 	FILE *parameter, *particles; 
-	if ((parameter = fopen(file1,"r"))==NULL) {
-		fprintf(stderr, "Could not open %s. Please Check your file.\n", file1);
+	if ((particles = fopen(file1,"r"))==NULL) {
+		fprintf(stderr, "Could not open %s. Please Check your file.\n", file2);
 		exit(EXIT_FAILURE); 
 	}
-	if ((particles = fopen(file2,"r"))==NULL) {
-		fprintf(stderr, "Could not open %s. Please Check your file.\n", file2);
+	if ((parameter = fopen(file2,"r"))==NULL) {
+		fprintf(stderr, "Could not open %s. Please Check your file.\n", file1);
 		exit(EXIT_FAILURE); 
 	}
 
