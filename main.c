85fc78c3eed4d08984b16c608e212e9432eab3b6
Plattenschieber <plattenschieber@gmail.com>
2013-07-10 18:18:26 +0200

    handle command line arguments

--- main.c
+++ main.c
@@ -1,9 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "list.h"
 #include "world.h"
 
-int main() {
+int main(int argc, char **argv) {
+	/* argument handling */
+	char file1[100];
+	char file2[100];
+	if (argc == 1) {
+		strcpy(file1,"unit.parameter");
+		strcpy(file2,"unit.particles");
+		printf("No parameter file, nor particles given - using 'unit.parameter' and 'unit.particles' instead\n");
+	}
+	else if (argc == 2) { 
+		strcpy(file1,argv[2]);
+		strcpy(file2, "unit.parameter");
+		printf("No parameter file given, using 'unit.parameter' instead\n");
+	}
+	else if (argc == 3) { 
+		strcpy(file1,argv[2]);
+		strcpy(file2,argv[3]);
+	}
+	else 
+		printf("Too many arguments.\n");
 
 
 	/* initialize the world and close files */
