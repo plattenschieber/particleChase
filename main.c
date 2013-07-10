6fe0f39b69853c23b43a57e5dc558ce46b8bae38
Plattenschieber <plattenschieber@gmail.com>
2013-07-10 18:17:41 +0200

    particles and parameter are set by input files

--- main.c
+++ main.c
@@ -4,20 +4,10 @@
 #include "world.h"
 
 int main() {
-	PARTICLE p1;
-   	PARTICLE p2;
-	WORLD *W = world_init(NULL);
 
-	/* insert first particle */
-	p1.x[0] = 1.3; p1.x[1] = 2.3;
-	p1.ID = 2;
-	list_insert(W->cells, NULL, &p1);
 
-	/* insert second particle on first place */
-	p2.x[0] = 1.5; p2.x[1] = 4.8;
-	p2.ID = 1;
-	list_insert(W->cells,W->cells->first, &p2);
-
+	/* initialize the world and close files */
+	WORLD *W = world_init(parameter, particles);
 	/* print particles before and after the simulation */ 
 	print_particles(W->cells);
 	simulate(W);
