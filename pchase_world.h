69a7782da89599e732209a3f6b4f317abfb2b1ca
Plattenschieber <plattenschieber@gmail.com>
2013-08-16 16:04:25 +0200

    update comments

--- pchase_world.h
+++ pchase_world.h
@@ -22,46 +22,57 @@ typedef struct {
                     pchase_world_t;
 
 /** initialize a world with static parameter
- * \return		a ready to use world
+ *
+ * \param[in] p4est a forest we want to integrate into our world
+ * \return	        a ready to use world
  */
 pchase_world_t     *pchase_world_init(p4est_t * p4est);
 
 /** start the simulation
+ *
  * \param [in] W	the world we are working on
  */
 void                pchase_world_simulate(pchase_world_t * W);
 
 /** update positions
+ *
  * \param [in] W	the world we are working on
  */
 void                pchase_world_update_x(pchase_world_t * W);
 
-/** return a particle via random distribution inside the pchase_worlds boundaries
+/** generate a particle via random distribution inside the pchase_worlds boundaries
+ *
  * \param [in] W	the world we are working on
+ * \return          generated particle inside the world
  */
 pchase_particle_t  *pchase_world_random_particle(pchase_world_t * W);
 
-/** insert a particle into its belonging quadrant and return the quadrant
+/** insert a particle into its belonging quadrant
+ *
  * \param [in] W	the world into which we are operating
  * \param [in] p	the particle to be inserted
+ * \return          return the mini quad where the particle stays
  */
 p4est_quadrant_t   *pchase_world_insert_particle(pchase_world_t * W, pchase_particle_t * p);
 
 /** prints out all particles into a XYZ file
+ *
  * \param [in] W	the world which shall be printed
  */
 void                pchase_world_print_particlesXYZ(pchase_world_t * W);
 
 /** take out a particle of its world and find out where it stays in p4est
+ *
  * \param [in] W	the world we are working on
  * \param [in] p	a particle lying inside the world
- * \return 		the quadrant to which the given particle belongs
+ * \return 		    the quadrant to which the given particle belongs
  */
 p4est_quadrant_t   *pchase_translate_particle_to_p4est(pchase_world_t * W, pchase_particle_t * p);
 
 /** find the owner of a given quadrant
+ *
  * \param [in] q	quadrant whos owner rank shall be found
- * \return 		owner rank
+ * \return 		    owner rank 
  */
 int                 pchase_quadrant_is_in_proc(p4est_quadrant_t * q);
 
