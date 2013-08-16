9cf540d060c5fdb3a453022449df2aa9e5e111c9
Plattenschieber <plattenschieber@gmail.com>
2013-08-16 22:23:17 +0200

    add some DEBUG messages

--- pchase_world.c
+++ pchase_world.c
@@ -60,7 +60,11 @@ pchase_world_random_particle(pchase_world_t * W)
         for (i = 0; i < DIM; i++)
                 p->x[i] = W->length[i] * rand() / (RAND_MAX + 1.);
 #ifdef DEBUG
+        printf("Random Particle Generator: ");
         p->ID = W->n_particles;
+        for (i = 0; i < DIM; i++)
+                printf("p->x[%d]=%lf,\t", i, p->x[i]);
+        printf("\n");
 #endif
         W->n_particles++;
         return p;
@@ -78,6 +82,11 @@ pchase_world_insert_particle(pchase_world_t * W, pchase_particle_t * p)
 
         /* actually create mini quadrant that is enclosing the given partile p */
         pchase_translate_particle_to_p4est(W, p, q);
+
+#ifdef DEBUG
+        printf("Insert Particle: particle(x,y)=(%lf,%lf)\n", p->x[0], p->x[1]);
+        printf("quadrant(x,y)=(%d,%d)\n", q->x, q->y);
+#endif
 
         /*
          * check if the quadrant holding our to be inserted particle lies on
@@ -100,6 +109,14 @@ void
 pchase_translate_particle_to_p4est(pchase_world_t * W, pchase_particle_t * p, p4est_quadrant_t * q)
 {
         double              quadrant_length = (double)(1 << P4EST_QMAXLEVEL);
+
+#ifdef DEBUG
+        printf("Step 1: p->x = %lf\n", p->x[0]);
+        printf("Step 2: p->x/W->length.x = %lf\n", p->x[0] / W->length[0]);
+        printf("Step 3: times quad_len = %lf with quad_len = %lf and root_len = %lld\n", p->x[0] / W->length[0] * quadrant_length, quadrant_length, P4EST_ROOT_LEN);
+        printf("Step 4: truncate = %d\n", (p4est_qcoord_t) (p->x[0] / W->length[0] * quadrant_length));
+        printf("Step 5: q->x = %d\n", (p4est_qcoord_t) (p->x[0] / W->length[0] * quadrant_length) << 1);
+#endif
 
         /*
          * normalize particle and transform it to p4est world length by
@@ -133,7 +150,9 @@ search_fn(p4est_t * p4est, p4est_topidx_t which_tree,
         p4est_quadrant_t   *q;
         int                 quadrant_length;
 
-        printf("HELLOOO I AM SEARCH in x:%i y%i\n", quadrant->x, quadrant->y);
+#ifdef DEBUG
+        printf("HELLOOO I AM A CALLBACK in x:%i y%i\n", quadrant->x, quadrant->y);
+#endif
 
         quadrant_length = P4EST_QUADRANT_LEN(quadrant->level);
         q = (p4est_quadrant_t *) point;
@@ -142,11 +161,14 @@ search_fn(p4est_t * p4est, p4est_topidx_t which_tree,
             q->y >= quadrant->y && q->y <= quadrant->y + quadrant_length &&
             /* q->p.user_int < quadrant->level) { */
             1) {
+#ifdef DEBUG
+                printf("YES YES YES - we found a quadrant whos child holds our mini quad\n");
+#endif 
+
                 /*
                  * replace current_quad with quadrant and save it's level for
                  * faster access in user_int
                  */
-
                 q->p.user_int = quadrant->level;
                 return 1;
         }
