0acfa4a64eea17020b1f79526f2efda9a31831d5
Plattenschieber <plattenschieber@gmail.com>
2013-08-17 18:09:07 +0200

    build the world before p4est and asign p4est afterwards
    all p4est callbacks are organized by the world

--- particleChase.c
+++ particleChase.c
@@ -43,6 +43,8 @@ main(int argc, char **argv)
         sc_init(mpi->mpicomm, 1, 1, NULL, SC_LP_ALWAYS);
         /* Registers p4est with the SC Library and sets the logging behavior */
         p4est_init(NULL, SC_LP_DEFAULT);
+        /* build up the world */
+        W = pchase_world_init(p4est);
 
         /* store connectivity for a unitsquare */
         connectivity = p4est_connectivity_new_unitsquare();
@@ -51,11 +53,11 @@ main(int argc, char **argv)
          * each
          */
         p4est = p4est_new_ext(mpi->mpicomm, connectivity, 0, 1, 1,
-                              sizeof(pchase_quadrant_data_t), NULL, NULL);
+                          sizeof(pchase_quadrant_data_t), W->init_fn, NULL);
         p4est_vtk_write_file(p4est, NULL, "pchase_new");
 
-        /* build up the world to insert one particle */
-        W = pchase_world_init(p4est);
+        /* don't forget to assign newly allocated p4est to the world */
+        W->p4est = p4est;
         pchase_world_insert_particle(W, pchase_world_random_particle(W));
 
         /* destroy the p4est and its connectivity structure */
