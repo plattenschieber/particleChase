9bf5dc15baafa08c3bb2b8c534ed9f3010b4e12b
Plattenschieber <plattenschieber@gmail.com>
2013-08-19 00:17:10 +0200

    tidy up main

--- particleChase.c
+++ particleChase.c
@@ -19,12 +19,10 @@ typedef struct {
 int
 main(int argc, char **argv)
 {
-
         int                 mpiret;
         mpi_context_t       mpi_context, *mpi = &mpi_context;
         p4est_t            *p4est;
         p4est_connectivity_t *connectivity;
-        p4est_refine_t      refine_fn;
         pchase_world_t     *W;
 
         /* initialize MPI and p4est internals */
@@ -36,10 +34,7 @@ main(int argc, char **argv)
         mpiret = MPI_Comm_rank(mpi->mpicomm, &mpi->mpirank);
         SC_CHECK_MPI(mpiret);
 
-        /*
-         * Sets the global program identifier (e.g. the MPI rank) and some
-         * flags
-         */
+        /* Sets global program identifiers (e.g. the MPIrank) and some flags */
         sc_init(mpi->mpicomm, 1, 1, NULL, SC_LP_ALWAYS);
         /* Registers p4est with the SC Library and sets the logging behavior */
         p4est_init(NULL, SC_LP_DEFAULT);
@@ -48,16 +43,14 @@ main(int argc, char **argv)
 
         /* store connectivity for a unitsquare */
         connectivity = p4est_connectivity_new_unitsquare();
-        /*
-         * build up uniform tree and require quadrant space for 25 particles
-         * each
-         */
+        /* build uniform tree and get space for 25 particles each */
         p4est = p4est_new_ext(mpi->mpicomm, connectivity, 0, 1, 1,
                           sizeof(pchase_quadrant_data_t), W->init_fn, NULL);
         p4est_vtk_write_file(p4est, NULL, "pchase_new");
 
         /* don't forget to assign newly allocated p4est to the world */
         W->p4est = p4est;
+
         /* add one particle to the world */
         pchase_world_insert_particle(W, pchase_world_random_particle(W));
 
@@ -66,14 +59,13 @@ main(int argc, char **argv)
         p4est_iterate(W->p4est, NULL, NULL, W->viter_fn, NULL, NULL);
 #endif
 
-        /* destroy the p4est and its connectivity structure */
+        /* destroy all particles, p4est and its connectivity structure */
         p4est_iterate(W->p4est, NULL, NULL, W->destroy_fn, NULL, NULL);
         p4est_destroy(p4est);
         p4est_connectivity_destroy(connectivity);
 
         /* clean up and exit */
         sc_finalize();
-
         mpiret = MPI_Finalize();
         SC_CHECK_MPI(mpiret);
 
