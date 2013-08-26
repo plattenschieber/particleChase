48c7ce2c113b98a265c0d4047bcc30bdde86e116
Plattenschieber <plattenschieber@gmail.com>
2013-08-26 16:02:42 +0200

    REMOVE send status and recv request

--- pchase_world.c
+++ pchase_world.c
@@ -340,10 +340,8 @@ pchase_world_insert_particles(pchase_world_t * W)
         /* do something with senders */
 
         /* handle all requests */
-        MPI_Request        *recv_request = P4EST_ALLOC(MPI_Request, W->p4est->mpisize);
         MPI_Request        *send_request = P4EST_ALLOC(MPI_Request, W->p4est->mpisize);
         MPI_Status         *recv_status = P4EST_ALLOC(MPI_Status, W->p4est->mpisize);
-        MPI_Status         *send_status = P4EST_ALLOC(MPI_Status, W->p4est->mpisize);
         pchase_particle_t  *recv_buf, *send_buf;
         int                 recv_count, recv_length, flag;
 
