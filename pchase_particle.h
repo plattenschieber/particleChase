7111ac0082336c733850e9b65e85d7547e2e49e5
Jeronim Morina <plattenschieber@gmail.com>
2013-10-25 11:55:49 +0200

    comment on "empirical" explanation of the number 25

--- pchase_particle.h
+++ pchase_particle.h
@@ -18,6 +18,11 @@ typedef struct {
  *              "big" simulations with 10^20, since the adaptive refinement
  *              (and accompanied by that the 2:1 relationship of the octants) 
  *              secures a partition fine enough to hold all particles.
+ * 
+ *              [of course there is an upper bound, where this won't work 
+ *              any more - the finest possible refinement level 
+ *              P4EST_QMAXLEVEL is actually set to 29
+ *              => smalles cube has length 1, and biggest 2^29]
  */
 typedef struct {
         int                 nParticles;
